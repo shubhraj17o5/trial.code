e4f0aea124c919013302192cc1d6608972fea556
ShubhRaj <187618408+shubhraj17o5@users.noreply.github.com>
2025-05-31 11:41:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,50 @@
-# print('hello print')
+print('hello print')
 
-# def add_numbers(a, b):
-#     sum = a + b
-#     return sum  # Return the sum
+def add_numbers(a, b):
+    sum = a + b
+    return sum  # Return the sum
 
-# # Call the function and print the result
-# result = add_numbers(1, 2)
-# print(f'The sum is {result}.')
+# Call the function and print the result
+result = add_numbers(1, 2)
+print(f'The sum is {result}.')
 
-# def greet(name):
-#     print(f'hello {name}')
+def greet(name):
+    print(f'hello {name}')
 
-# greet("alice")
+greet("alice")
 
-# a=(int(input('Enter an integer:')))
-# b=(float(input('enter a float:')))
-# print(a + b)
+a=(int(input('Enter an integer:')))
+b=(float(input('enter a float:')))
+print(a + b)
 
-# import calendar 
-# year= int(input('enter the year:'))
-# month= int(input('enter the month(1-12):'))
-# print(calendar.month(year, month))
+import calendar 
+year= int(input('enter the year:'))
+month= int(input('enter the month(1-12):'))
+print(calendar.month(year, month))
 
-# import datetime
+import datetime
 
-# def greet_user():
-#     # Get the current time
-#     now = datetime.datetime.now()
-#     current_hour = now.hour
+def greet_user():
+    # Get the current time
+    now = datetime.datetime.now()
+    current_hour = now.hour
 
-#     # Determine the greeting based on the current hour
-#     if 5 <= current_hour < 12:
-#         greeting = "Good Morning!"
-#     elif 12 <= current_hour < 17:
-#         greeting = "Good Afternoon!"
-#     elif 17 <= current_hour < 21:
-#         greeting = "Good Evening!"
-#     else:
-#         greeting = "Good Night!"
+    # Determine the greeting based on the current hour
+    if 5 <= current_hour < 12:
+        greeting = "Good Morning!"
+    elif 12 <= current_hour < 17:
+        greeting = "Good Afternoon!"
+    elif 17 <= current_hour < 21:
+        greeting = "Good Evening!"
+    else:
+        greeting = "Good Night!"
 
-#     return greeting
+    return greeting
 
-# if __name__ == "__main__":
-#     print(greet_user())
+if __name__ == "__main__":
+    print(greet_user())
 
-# print(12345)
+print(12345)
 
 n = 7  # Height of D
 
