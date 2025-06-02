759b7ce1dc7d56695a2bd78672fc1963151c3043
ShubhRaj <187618408+shubhraj17o5@users.noreply.github.com>
2025-06-02 12:21:30 +0530

    removed the previous code

--- project.py
+++ project.py
@@ -1,6 +1,2 @@
 # write a python prog to print a number 1to10
 print(1,2,3,4,5,6,7,8,9,0)
-print("hello world")
-# This is a simple Python program to print numbers from 1 to 10
-for i in range(1, 11):
-    print(i, end=' ')
